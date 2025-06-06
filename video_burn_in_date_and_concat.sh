d0976051a553caecada1806a48c60d1a12f3ce14
Sandro <sandrospeh@gmail.com>
2025-06-06 18:23:25 +1000

    Added timezone difference parameter

--- video_burn_in_date_and_concat.sh
+++ video_burn_in_date_and_concat.sh
@@ -1,9 +1,15 @@
 #! /usr/bin/bash
 
+TIME_DIF_HOURS=8
+
 # get capitzalized creation date of each file and add drawtext filter containing creation date
 for f in *.MP4 ; do 
-	FILE_CREATION_DATE=`date +%s -r $f`000000;
-	ffmpeg -y -i $f -vf "drawtext=expansion=strftime:basetime=${FILE_CREATION_DATE}:text='%H\\:%M %d. %b %Y':fontsize=(h/35):fontcolor='white@0.4':x=w-tw-50:y=h-th-50" "${f%%.*}_timecoded.mkv"
+	FILE_CREATION_DATE_ORIGINAL=`date +%s -r $f`;
+
+	TIME_DIF_SECONDS=$((TIME_DIF_HOURS * 3600))
+	FILE_CREATION_DATE=$((FILE_CREATION_DATE_ORIGINAL + TIME_DIF_SECONDS))000000
+
+	ffmpeg -y -i $f -vf "drawtext=expansion=strftime:basetime=${FILE_CREATION_DATE}:text='%H\\:%M %d. %b %Y':fontsize=24:fontcolor='white@0.4':x=w-tw-50:y=h-th-50" "${f%%.*}_timecoded.mkv"
 done;
 
 touch list.txt
